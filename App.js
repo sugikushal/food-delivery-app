f6290a49b8efc15047ac28cc3b86fefa345f4a3e
Suganthi <sugikushal@gmail.com>
2023-08-18 09:26:49 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+const parent = React.createElement(
+    "div", {id: "parent"},
+    [React.createElement(
+        "div", {id: "child1"}, 
+        [React.createElement(
+            "h1", {id: "heading"}, "Hello hello"
+        ), React.createElement(
+            "h2", {id: "heading2"}, "Hello hello2"
+        )]
+    ), React.createElement(
+        "div", {id: "child2"}, 
+        [React.createElement(
+            "h1", {id: "heading11"}, "Hello hello"
+        ), React.createElement(
+            "h2", {id: "heading12"}, "Hello hello2"
+        )]
+    )]
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
